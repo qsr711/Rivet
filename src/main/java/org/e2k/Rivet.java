d53a4eadc0cb628cfc24be687dfac1cd52c82be8
qsr711 <qsr7.11@gmail.com>
2023-07-20 10:34:47 +0200

    Updated Build version

--- src/main/java/org/e2k/Rivet.java
+++ src/main/java/org/e2k/Rivet.java
@@ -48,7 +48,7 @@ public class Rivet {
 	private DisplayView display_view;
 	private static Rivet theApp;
 	private static DisplayFrame window;
-	public final String program_version="Rivet (Build 91) Beta 3";
+	public final String program_version="Rivet (Build 91)";
 	public int vertical_scrollbar_value=0;
 	public int horizontal_scrollbar_value=0;
 	public boolean pReady=false;
